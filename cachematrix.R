e0c15f9cb6f061537dea9dad89d3a6842ca94f24
Sabu Varghese <sabutvarghese@yahoo.com>
2014-10-22 22:22:34 -0400

    Date 10/22/2014 Description: Assignment submitted by Sabu Varghese for R Programming course rprog-008

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,79 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
+  ##============================================================================
+  ## Author: Sabu Varghese
+  ## Date: 10/22/2014
+  ## Function Arguments:
+  ##     x - matrix
+  ##
+  ## Purpose: Cache the matrix passed as argument and returns a list that
+  ## contains setters/getters for matrix and its inverse
+  ##============================================================================
 
+  matInverse <- NULL
+  
+  ## Set function for the new matrix. Assign the input matrix to x and reset the
+  ## value of matInverse; note that when calling the set function from outside,
+  ## variables will be in the enclosing environment, hence <<-
+  set <- function(y) {
+    x <<- y
+	matInverse <<- NULL 
+  }
+
+  ## Get function; returns the value of matrix in the cache
+  get <- function() { x }
+
+  ## Assign inverse of the matrix to setMatInverse; note that when calling the 
+  ## set function from outside, variables will be in the enclosing environment,
+  ## hence <<-
+  setMatInverse <- function(mInv) {
+    matInverse <<- mInv 
+  }
+
+  ## Get function; returns the value of matInverse
+  getMatInverse <- function() {
+    matInverse 
+  }
+
+  ## Create list containing setters/getters
+  list(
+    set = set, 
+    get = get,
+    setMatInverse = setMatInverse,
+    getMatInverse = getMatInverse
+	)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ##============================================================================
+  ## Author: Sabu Varghese
+  ## Date: 10/22/2014
+  ## Function Arguments:
+  ##     x - list; result from makeCacheMatrix()
+  ##
+  ## Purpose: Get a special vector of setters/getters from the makeCacheMatrix.
+  ## If inverse is already calculated for the matrix, return the inverse from 
+  ## memory cache. If not already calculated, find and return the inverse of the
+  ## matrix. 
+  ##============================================================================
+  
+  mi <- NULL
+  
+  ## See if the inverse of the matrix exists in cache
+  matInverse <- x$getMatInverse()
+  
+  ## If inverse of the matrix exists in cache, assign to mi; else compute
+  ## the inverse of the matrix using solve() function and assign the result to mi
+  if (!is.null(matInverse)) { 
+    print ("Inverse already calculated for this matrix...getting cached inverse")
+    mi <- matInverse
+  } else {
+    mat <- x$get()
+    matInverse <- solve(mat, ...)
+    x$setMatInverse(matInverse)
+    mi <- matInverse
+  }
+
+  ## Return the inverse of the matrix
+  return (mi)
+  
 }
